98eefa1ecab41081eedf701ee3674521d0fc1a20
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2021-10-27 13:56:51 +0100

    connexion à la bd

--- projet/etudiants.php
+++ projet/etudiants.php
@@ -8,6 +8,13 @@
 <?php
 // script php pour se connecter à la base
 
+$mysqli = new mysqli("localhost", "root", "", "ippsi");
+if ($mysqli->connect_errno) {
+    echo "Échec lors de la connexion à MySQL : (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+else{
+    echo "Connexion réussi!";
+}
 
 // script pour l'affichage des data depuis la base
 ?>
