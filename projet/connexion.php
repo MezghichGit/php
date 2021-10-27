1827a7fc00fae01b59373d42bc2a6d23d8e97b00
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2021-10-27 15:17:53 +0100

    page addEtudiant.html

--- projet/connexion.php
+++ projet/connexion.php
@@ -4,7 +4,7 @@ if ($mysqli->connect_errno) {
     echo "Échec lors de la connexion à MySQL : (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
 else{
-    echo "Connexion réussi!";
+    //echo "Connexion réussi!";
 }
 
 ?>
