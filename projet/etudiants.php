e354bd3067399d613e2e537e80088cd2336d84fd
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2021-10-27 13:52:22 +0100

    projet gestion ecole

---
+++ projet/etudiants.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+</head>
+
+<body>
+ <h1 align="center">Bienvenue à notre école IPSSI</h1>
+<?php
+// script php pour se connecter à la base
+
+
+// script pour l'affichage des data depuis la base
+?>
+</body>
+</html>
