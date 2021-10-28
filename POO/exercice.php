0a001d55b85674a12c2666da14367d074a857d33
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2021-10-28 14:25:01 +0100

    classe Cours

---
+++ POO/exercice.php
@@ -0,0 +1,80 @@
+<?php
+class Cours
+{
+    private $id;
+    private $libelle;
+    private $vh;
+
+    public function __construct($libelle,$vh)
+    {
+        $this->libelle = $libelle;
+        $this->vh = $vh;
+        
+    }
+    
+
+    /**
+     * Get the value of libelle
+     */ 
+    public function getLibelle()
+    {
+        return $this->libelle;
+    }
+
+    /**
+     * Set the value of libelle
+     *
+     * @return  self
+     */ 
+    public function setLibelle($libelle)
+    {
+        $this->libelle = $libelle;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */ 
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of vh
+     */ 
+    public function getVh()
+    {
+        return $this->vh;
+    }
+
+    /**
+     * Set the value of vh
+     *
+     * @return  self
+     */ 
+    public function setVh($vh)
+    {
+        $this->vh = $vh;
+
+        return $this;
+    }
+}
+
+
+$monCours = new Cours("PHP",16); // création d'objet Cours
+
+echo $monCours->getLibelle()." ".$monCours->getVh();
